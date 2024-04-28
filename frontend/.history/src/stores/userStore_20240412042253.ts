f649503b86597c14ade51a2ef9d3562f39d40119
supakorn <supakorn0728@gmail.com>
2024-04-28 20:15:30 +0700

    update

--- frontend/.history/src/stores/userStore_20240412042253.ts
+++
@@ -1,3 +0,0 @@
-import { create } from "zustand"
-import { persist } from "zustand/middleware"
-import {User} from "../gql/graphql"
--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -7,8 +7,39 @@ import {
   ApolloLink,
   split
 } from '@apollo/client'
+import {WebSocketLink} from "@apollo/client/link/ws"
 import { createUploadLink} from 'apollo-upload-client'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { loadDevMessages, logDevMessage } from '@apollo/client/dev'
 import { useUserStore } from './stores/userStore'
 
+
+async function refreshToken(client: ApolloClient<NormalizedCacheObject>) {
+  try {
+    const { data } = await client.mutate({
+      mutation: gql`
+      mutation RefreshToken {
+        refreshToken
+      }
+      `,
+    }) 
+    const newAccessToken = data?.refreshToken
+    if (!newAccessToken) {
+      throw new Error("New access token not received.")
+    }
+  } catch (err) {
+    throw new Error("Error getting new access token.")
+  }
+}
+
+const retryCount = 0
+const maxRetry = 3
+const wsLink = new WebSocketLink({
+  url: `ws://localhost:3000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+    }
+  }
+})
